7e75ede7a4c8049f241534175c5b3b626549643c
maikol09 <maikol.araya@gmail.com>
2012-12-02 16:52:34 -0600

    init

--- HelloFacebookSample/HelloFacebookSample/CarnesViewController.h
+++ HelloFacebookSample/HelloFacebookSample/CarnesViewController.h
@@ -1,11 +1,17 @@
-//
-//  CarnesViewController.h
-//  HelloFacebookSample
-//
-//  Created by Maikol Araya on 12/1/12.
-//
-//
-
+/*
+ APPetitoso is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+ 
+ APPetitoso is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with APPetitoso.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #import <UIKit/UIKit.h>
 
 @interface CarnesViewController : UIViewController{
